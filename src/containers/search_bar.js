830c5fc38db8619bc3ee8b38b0908e9ac77719cf
Sachin <sachinb443@gmail.com>
2018-06-12 13:26:40 +0530

    Middleware added

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,10 +1,27 @@
 import React , { Component } from 'react';
 
 export default class SearchBar extends Component{
-    render(){
+    constructor(props){
+        super(props);
+    
+        this.state = ({term: ''})
+    }
+
+    onInputChange=(event)=> {
+        console.log(event.target.value);
+        this.setState({term: event.target.value})
+    }
+
+    onSubmitForm=(event) => {
+        event.preventDefault();
+    }
+
+    render() {
         return(
-            <form className="input-group">
-                <input />
+            <form onSubmit={this.onSubmitForm} className="input-group">
+                <input placeholder="Get forecast for 5 days of your favourite city" 
+                className="form-control"
+                value = {this.state.term} onChange = {this.onInputChange.bind(this)} />
                 <span className="input-group-btn">
                     <button type="submit" className="btn btn-secondary">Submit</button>
                 </span>
